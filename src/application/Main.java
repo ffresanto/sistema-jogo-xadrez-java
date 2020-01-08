4d9c05b7267bf41412eb1e59fbcebee57554297e
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2020-01-08 19:56:40 -0300

    Classe posicao criada

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,12 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Posicao pos = new Posicao(3, 5);
+        System.out.println(pos);
     }
 }
