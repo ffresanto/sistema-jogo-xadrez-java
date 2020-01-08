4478441f14752964063f057ed61b857b182144bb
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2020-01-08 20:03:20 -0300

    Recriada classe Posicao

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,12 @@
 package application;
 
-import tabuleiro.Posicao;
+import jogotabuleiro.Posicao;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Posicao pos = new Posicao(3, 5);
+        Posicao pos = new Posicao(3,4);
         System.out.println(pos);
     }
 }
