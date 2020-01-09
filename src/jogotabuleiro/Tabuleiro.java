ac1e3ecaeab4ddc6095ae380a91d2146dd339c10
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2020-01-09 20:15:39 -0300

    Criada exception personalizada

--- src/jogotabuleiro/Tabuleiro.java
+++ src/jogotabuleiro/Tabuleiro.java
@@ -7,6 +7,9 @@ public class Tabuleiro {
     private Peca[][] pecas;
 
     public Tabuleiro(int linhas, int colunas) {
+        if (linhas < 1 || colunas < 1){
+            throw new TabuleiroException("Erro de criação tabuleiro: É necessario que tenha pelomenos uma linha e uma coluna");
+        }
         this.linhas = linhas;
         this.colunas = colunas;
         pecas = new Peca[linhas][colunas];
@@ -16,29 +19,45 @@ public class Tabuleiro {
         return linhas;
     }
 
-    public void setLinhas(int linhas) {
-        this.linhas = linhas;
-    }
-
     public int getColunas() {
         return colunas;
     }
 
-    public void setColunas(int colunas) {
-        this.colunas = colunas;
-    }
-
     public Peca peca(int linha, int coluna){
+        if (!existePosicao(linha, coluna)){
+            throw new TabuleiroException("Posição fora do tabuleiro");
+        }
         return pecas[linha][coluna];
     }
 
     public Peca peca(Posicao posicao){
+        if (!existePosicao(posicao)){
+            throw new TabuleiroException("Posição fora do tabuleiro");
+        }
         return pecas[posicao.getLinha()][posicao.getColuna()];
     }
 
     public void lugarPeca(Peca peca, Posicao posicao){
+        if (existePeca(posicao)){
+            throw new TabuleiroException("Já existe uma peça nessa posição " + posicao);
+        }
         pecas[posicao.getLinha()][posicao.getColuna()] = peca;
         peca.posicao = posicao;
+    }
+
+    private boolean existePosicao(int linha, int coluna){
+        return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+    }
+
+    public boolean existePosicao(Posicao posicao){
+        return existePosicao(posicao.getLinha(), posicao.getColuna());
+    }
+
+    public boolean existePeca(Posicao posicao){
+        if (!existePosicao(posicao)){
+            throw new TabuleiroException("Posição fora do tabuleiro");
+        }
+        return peca(posicao) != null;
     }
 
 }
