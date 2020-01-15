ee3653df21328ef84c5b0c705bf0f89ffcc7423c
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2020-01-14 21:24:57 -0300

    Bispo

---
+++ src/xadrez/pecas/Bispo.java
@@ -0,0 +1,67 @@
+package xadrez.pecas;
+
+import jogotabuleiro.Posicao;
+import jogotabuleiro.Tabuleiro;
+import xadrez.Cor;
+import xadrez.PecaXadrez;
+
+public class Bispo extends PecaXadrez {
+
+    public Bispo(Tabuleiro tabuleiro, Cor cor) {
+        super(tabuleiro, cor);
+    }
+
+    @Override
+    public String toString() {
+        return "B";
+    }
+
+    @Override
+    public boolean[][] possivelMovimentos() {
+        boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+        Posicao p = new Posicao(0,0);
+
+        // noroeste
+        p.setValores(posicao.getLinha() - 1, posicao.getColuna() -1);
+        while (getTabuleiro().existePosicao(p) && !getTabuleiro().existePeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha() - 1, p.getColuna() - 1);
+        }
+        if (getTabuleiro().existePosicao(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        // nordeste
+        p.setValores(posicao.getLinha() -1 , posicao.getColuna() + 1);
+        while (getTabuleiro().existePosicao(p) && !getTabuleiro().existePeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha() - 1 , p.getColuna() + 1);
+        }
+        if (getTabuleiro().existePosicao(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        // sudeste
+        p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+        while (getTabuleiro().existePosicao(p) && !getTabuleiro().existePeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha() + 1, p.getColuna() + 1);
+        }
+        if (getTabuleiro().existePosicao(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        // sudoeste
+        p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+        while (getTabuleiro().existePosicao(p) && !getTabuleiro().existePeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha() + 1, p.getColuna() - 1);
+        }
+        if (getTabuleiro().existePosicao(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        return mat;
+    }
+}
